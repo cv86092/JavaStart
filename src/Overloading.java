02d6b818abd8f07176a747d339b63a24b29233b9
cv86092 <cv86092@gmail.com>
2016-08-22 23:42:40 +0800

    add overloading

--- src/Overloading.java
+++ src/Overloading.java
@@ -1,32 +1,41 @@
-class Ttest{
-	int  x ;
-	int  y ;
-	
-	Ttest(int intx){
-		x=  intx;
-		
-	}
-	
-	Ttest(){
-		
-		
-	}
-	
-	
-	void show(){
-		System.out.println("x=" +x);
-		System.out.println("y=" +y);
-		
-	}
-}
+ class Overloading{
+	 public static void main(String[] argv){
+		 System.out.println("123");
+		 
+		 showDog a = new showDog(1,"lucky");
+		 a.showDog();
+		 showDog.hellocat();
+		 a.hellocat();
 
+		 
+	 }
+ }
+ 
+ class showDog{
+	 String dog;
+	 String cat;
+	 
+	 showDog(){
+		 
+	 }
+	 
+	 showDog(String dogref){
+		 this.dog=dogref;
+		 
+	 }
+	 
+	 showDog(int i,String catref){
+		 this.cat=catref;
+	 }
+	 
+	 void showDog(){
+		 System.out.println(dog);
+		 System.out.println(cat);
 
-public class Overloading {
-
-	public static void mian(String[] argv){
-		Ttest a = new Ttest();
-		a.show();
-		
-	}
-
-}
+	 }
+	 
+	 static void hellocat(){
+		 System.out.println("hello every cat");
+	 }
+	 
+ }
